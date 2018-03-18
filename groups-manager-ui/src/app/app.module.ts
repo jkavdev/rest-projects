29f4eebd03b8478e4393cae6603d173fa0b93089
jkavdev <jhonatankolen@gmail.com>
2018-03-18 20:40:44 -0300

    006 - adicionando dica e responsividade a tabela de grupos

--- groups-manager-ui/src/app/app.module.ts
+++ groups-manager-ui/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { InputTextModule } from 'primeng/inputtext';
 import { ButtonModule } from 'primeng/button';
 import { DataTableModule } from 'primeng/datatable';
+import { TooltipModule } from 'primeng/tooltip';
 import { CalendarModule } from 'primeng/calendar';
 
 import { AppComponent } from './app.component';
@@ -19,6 +20,7 @@ import { AppComponent } from './app.component';
     CalendarModule,
     ButtonModule,
     DataTableModule,
+    TooltipModule,
     InputTextModule
   ],
   providers: [],
