41b840506fed4416ca7ecf8a0e7794e5d3cd9a81
jkavdev <jhonatankolen@gmail.com>
2018-03-18 20:01:28 -0300

    004 - adicionando form de pesquisa de grupos

--- groups-manager-ui/src/app/app.module.ts
+++ groups-manager-ui/src/app/app.module.ts
@@ -2,6 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {InputTextModule} from 'primeng/inputtext';
+import {ButtonModule} from 'primeng/button';
 import { CalendarModule } from 'primeng/calendar';
 
 import { AppComponent } from './app.component';
@@ -13,7 +15,9 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    CalendarModule
+    CalendarModule,
+    ButtonModule,
+    InputTextModule
   ],
   providers: [],
   bootstrap: [AppComponent]
