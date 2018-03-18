99a7baf59bc851625e7d8431a6047b188ff6f959
jkavdev <jhonatankolen@gmail.com>
2018-03-18 19:45:49 -0300

    002 - adicionando primeng

--- groups-manager-ui/src/app/app.module.ts
+++ groups-manager-ui/src/app/app.module.ts
@@ -1,16 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CalendarModule } from 'primeng/calendar';
 
 import { AppComponent } from './app.component';
-
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    CalendarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
