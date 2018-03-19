a789aacf2b2c5fdc6827058a359385d27f56afdb
jkavdev <jhonatankolen@gmail.com>
2018-03-19 02:14:36 -0300

    019 - adicionando validacoes nos formularios, e desabilitando o botao de salvar, ate que seja preenchido todos os campos obrigatorios

--- groups-manager-ui/src/app/evento-cadastro/evento-cadastro.component.ts
+++ groups-manager-ui/src/app/evento-cadastro/evento-cadastro.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { SelectItem } from 'primeng/api';
 
 @Component({
   selector: 'app-evento-cadastro',
@@ -8,20 +9,22 @@ import { Component, OnInit } from '@angular/core';
 export class EventoCadastroComponent implements OnInit {
 
   constructor() {
+    this.grupos = [
+      { label: 'Selecione o Grupo', value: null },
+      { label: 'GAM', value: 1 },
+      { label: 'Movimento', value: 2 }
+    ]
+    this.ufs = [
+      { label: 'Selecione o Estado', value: null },
+      { label: 'GO', value: 1 },
+      { label: 'DF', value: 2 }
+    ]
   }
 
   ngOnInit() {
   }
 
-  grupos: [
-    { label: 'Selecione o Grupo', value: null },
-    { label: 'GAM', value: 1 },
-    { label: 'Movimento', value: 2 }
-  ]
-  UFs: [
-    { label: 'Selecione o Estado', value: null },
-    { label: 'GO', value: 1 },
-    { label: 'DF', value: 2 }
-  ]
+  grupos: SelectItem[];
+  ufs: SelectItem[];
 
 }
--- groups-manager-ui/src/app/noticia-cadastro/noticia-cadastro.component.ts
+++ groups-manager-ui/src/app/noticia-cadastro/noticia-cadastro.component.ts
@@ -13,11 +13,11 @@ export class NoticiaCadastroComponent {
       { label: 'Mundo', value: { id: 2, name: 'Mundo' } },
       { label: 'Negócios', value: { id: 3, name: 'Negócios' } },
       { label: 'Entreterimento', value: { id: 4, name: 'Entreterimento' } },
-    ]
+    ];
     this.grupos = [
       { label: 'GAM', value: { id: 2, name: 'GAM' } },
       { label: 'Movimento', value: { id: 3, name: 'Movimento' } }
-    ]
+    ];
   }
 
   topicos: SelectItem[];
