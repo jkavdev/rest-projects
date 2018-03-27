2e3145084f7dfd690d461cafb3ca145d6d129e88
jkavdev <jhonatankolen@gmail.com>
2018-03-27 12:39:18 -0300

    065 - estudando a validacao de CPF

--- groups-manager/src/main/java/br/com/jkavdev/groups/domain/integrante/IntegranteController.java
+++ groups-manager/src/main/java/br/com/jkavdev/groups/domain/integrante/IntegranteController.java
@@ -5,10 +5,12 @@ import java.util.List;
 import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 
+import org.hibernate.validator.constraints.br.CPF;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -23,6 +25,8 @@ import br.com.jkavdev.groups.utils.ServiceMap;
 
 @RestController
 @RequestMapping("/integrantes")
+//necessario para realizar a validacao de parametros recebidos pela url
+@Validated
 public class IntegranteController  implements ServiceMap {
 
 	@Autowired
@@ -45,9 +49,23 @@ public class IntegranteController  implements ServiceMap {
 		publisher.publishEvent(new RecursoCriadoEvent(this, response, integranteSalvo.getId()));
 		return ResponseEntity.status(HttpStatus.CREATED).body(integranteSalvo);
 	}
+	/***
+	 * 
+	 * @param cpf
+	 * Como o hibernate tem o validador para o @CPF
+	 * Temos um servico que recebe uma string como CPF
+	 * Quando atribuido o valor ao parametro o hibernate validator
+	 * ira validar o valor, sendo obrigatorio um CPF
+	 * */
 	@PostMapping("/validarcpf")
+	@ResponseStatus(HttpStatus.OK)
 	public void validarCpf(@Valid @RequestBody IntegranteFilter filter) {
-		System.out.println(filter.getCpf());
+		System.out.println(filter.getCpf() + " eh valido");
+	}
+	@GetMapping("{cpf}/validar")
+	@ResponseStatus(HttpStatus.OK)
+	public void validarCpf(@CPF @PathVariable String cpf) {
+		System.out.println(cpf + " eh valido");
 	}
 
 }
